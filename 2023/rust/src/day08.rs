e9fde0860cb3df4ebe05f40af98f7d11ed5f90bf
Bibek <bibek@xenith.tech>
2023-12-08 20:12:33 +0545

    day 08 optimized

--- 2023/rust/src/day08.rs
+++ 2023/rust/src/day08.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
-use regex::Regex;
 use num::Integer;
+use regex::Regex;
 
 struct Instr {
     l: String,
@@ -49,11 +49,11 @@ fn gen_set(input: &String) -> Set {
     let re = Regex::new(r"\w{3}").unwrap();
     let mut map = HashMap::new();
     data[1].lines().for_each(|l| {
-        let vec = re
-            .find_iter(l)
-            .map(|c| c.as_str())
-            .collect::<Vec<&str>>();
-        map.insert(vec[0].to_string(), Instr::new(vec[1].to_string(), vec[2].to_string()));
+        let vec = re.find_iter(l).map(|c| c.as_str()).collect::<Vec<&str>>();
+        map.insert(
+            vec[0].to_string(),
+            Instr::new(vec[1].to_string(), vec[2].to_string()),
+        );
     });
 
     Set::new(map, dirs)
@@ -71,13 +71,12 @@ fn q2(input: &String) -> u64 {
         .map
         .keys()
         .filter(|k| Regex::new(r"A$").unwrap().is_match(k))
-        .map(|start| {
-            resolve(&set, start, false)
-        });
+        .map(|start| resolve(&set, start, false));
     list.into_iter().fold(1, |a, b| a.lcm(&b))
 }
 
 fn resolve(set: &Set, start: &str, zzz: bool) -> u64 {
+    let re = Regex::new(r"Z$").unwrap();
     let mut found = false;
     let mut curr = start;
     let mut count = 0usize;
@@ -87,7 +86,6 @@ fn resolve(set: &Set, start: &str, zzz: bool) -> u64 {
         curr = set.map.get(curr).unwrap().get(dir);
         count += 1;
 
-        let re = Regex::new(r"Z$").unwrap();
         match zzz {
             true => found = curr == "ZZZ",
             false => found = re.is_match(curr),
